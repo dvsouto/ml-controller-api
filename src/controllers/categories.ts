3f121f7e983f83359cd139c32064e8e3807ff060
Davi Souto <davi.souto@gmail.com>
2022-07-21 00:10:10 -0300

    feat: init categories dump

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -55,18 +55,33 @@ class CategoriesController {
 				const ml_response = await mercado_livre.categories.listAll();
 
 				const promises_categories = Object.keys(ml_response.data).map((key_category) => {
+					// eslint-disable-next-line no-async-promise-executor, @typescript-eslint/no-unused-vars
 					return new Promise(async (resolve, reject) => {
 						const category = ml_response.data[key_category];
-						const sub_categories = category.children_categories as Array<object>;
-	
-						const promises_sub_categories = sub_categories.map((sub_category) => {
-							return new Promise((resolve_sub_category, reject_sub_category) => {
-								// console.log(sub_category);
-								resolve_sub_category(sub_category);
-							});
-						});
+						const exists_sub_categories = (category.children_categories as Array<object>).length > 0;
+						// eslint-disable-next-line prefer-const
+						let has_parent_category = false;
+						
+						if ((category.path_from_root as Array<object>).length == 1 && (category.path_from_root as Array<object>)[0]["id"] == category.id) {
+							has_parent_category = true;
+						}
 
-						const result_sub_categories = await Promise.allSettled(promises_sub_categories);
+						if (has_parent_category) {
+							console.log(category);
+						}
+						// console.log(has_parent_category);
+						// const sub_categories = category.children_categories as Array<object>;
+
+						// console.log(category);
+						// const promises_sub_categories = sub_categories.map((sub_category) => {
+						// 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+						// 	return new Promise((resolve_sub_category, reject_sub_category) => {
+						// 		console.log(sub_category);
+						// 		resolve_sub_category(sub_category);
+						// 	});
+						// });
+
+						// const result_sub_categories = await Promise.allSettled(promises_sub_categories);
 						// console.log(result_sub_categories);
 						resolve(category);
 					});
