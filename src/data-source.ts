37ed835b7ed5f4f9cf9ca025a37c7c90a1391927
Davi Souto <davi.souto@gmail.com>
2022-07-22 03:57:53 -0300

    feat: CliDataSource v2

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 // import { User } from "./entity/User";
 // import { Environment } from "./utils/environment";
 
@@ -7,14 +7,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const dataSourceOptions = {
 	applicationName: "ml-controller-api",
 	type: "postgres",
-	host: process.env.PG_HOST,
-	port: parseInt(process.env.PG_PORT),
-	username: process.env.PG_USER,
-	password: process.env.PG_PASSWORD,
-	database: process.env.PG_DB,
 	schema: "public",
 	connectTimeoutMS: 5000,
 	synchronize: false,
@@ -23,4 +18,22 @@ export const AppDataSource = new DataSource({
 	entities: ["./src/entity/*.ts"],
 	migrations: ["./src/migrations/*.ts"],
 	subscribers: [],
-});
+};
+
+export const AppDataSource = new DataSource({
+	...dataSourceOptions,
+	host: process.env.PG_HOST,
+	port: parseInt(process.env.PG_PORT),
+	username: process.env.PG_USER,
+	password: process.env.PG_PASSWORD,
+	database: process.env.PG_DB,
+} as DataSourceOptions);
+
+export const CliDataSource = new DataSource({
+	...dataSourceOptions,
+	host: "localhost",
+	port: parseInt(process.env.PG_PORT),
+	username: process.env.PG_USER,
+	password: process.env.PG_PASSWORD,
+	database: process.env.PG_DB,
+} as DataSourceOptions);
