b92c75385629e67ebaff96545b969f994f2baf09
Davi Souto <davi.souto@gmail.com>
2022-07-22 03:59:13 -0300

    feat: category and subcategory entities

---
+++ src/entity/Category.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany, JoinColumn } from "typeorm";
+import { Subcategory } from "./Subcategory";
+
+@Entity('category')
+export class Category {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true })
+    ml_id: string;
+
+    @Column()
+    name: string;
+
+    @Column({ nullable: true })
+    picture: string;
+
+    @Column({ nullable: true })
+    permalink: string;
+
+    @OneToMany(type => Subcategory, subcategory => subcategory.category_ml_id)
+    @JoinColumn({ name: 'ml_id', referencedColumnName: "category_ml_id" })
+    subcategories: Promise<Subcategory[]>;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
---
+++ src/entity/Subcategory.ts
@@ -0,0 +1,36 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, JoinColumn, OneToOne } from "typeorm";
+import { Category } from "./Category";
+
+@Entity('subcategory')
+export class Subcategory {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true })
+    ml_id: string;
+
+    @Column()
+    category_ml_id: string;
+
+    @Column()
+    name: string;
+
+    @Column({ nullable: true })
+    picture: string;
+
+    @Column({ nullable: true })
+    permalink: string;
+
+    @Column({ default: true })
+    has_children: boolean;
+
+    @OneToOne(type => Category)
+    @JoinColumn({ name: 'category_ml_id', referencedColumnName: "ml_id" })
+    category: Promise<Category>;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
