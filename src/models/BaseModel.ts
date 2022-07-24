27daed6285d13b27c22c5a980c062a51543f8866
Davi Souto <davi.souto@gmail.com>
2022-07-24 02:44:10 -0300

    feat: add functions to BaseModel

--- src/models/BaseModel.ts
+++ src/models/BaseModel.ts
@@ -1,6 +1,7 @@
-import { DataSource, Entity, EntityOptions, EntityTarget, InsertResult, Repository } from "typeorm"; 
+import { DataSource, Entity, EntityOptions, EntityTarget, FindOneOptions, FindOptionsWhere, InsertResult, Repository } from "typeorm"; 
 import { AppDataSource } from "@src/data-source";
 import { IBaseModel } from "./interfaces";
+import _ from "lodash";
 
 class BaseModel implements IBaseModel {
 	declare entity: EntityTarget<{ (options?: EntityOptions): ClassDecorator; (name?: string, options?: EntityOptions): ClassDecorator; }>;
@@ -16,8 +17,40 @@ class BaseModel implements IBaseModel {
 		this.repository = AppDataSource.getRepository(this.entity);
 	}
 
+	public findAll(): Promise<typeof Entity[]>{
+		return this.repository.find();
+	}
+
+	public findOne(options: FindOneOptions): Promise<typeof Entity | null>{
+		return this.repository.findOne(options);
+	}
+
+	public findOneBy(where: FindOptionsWhere<typeof Entity> | FindOptionsWhere<typeof Entity>[]): Promise<typeof Entity | null>{
+		return this.repository.findOneBy(where);
+	}
+
 	public insert(data: object): Promise<InsertResult>{
 		return this.repository.insert(data);
+	}
+
+	public async insertOrUpdate(where: FindOptionsWhere<typeof Entity> | FindOptionsWhere<typeof Entity>[], data: object): Promise<any>{
+		const findData = await this.findOneBy(where);
+
+		if (findData) {
+			_.forOwn(data, (value, key) => {
+				findData[key] = value;
+			});
+
+			return this.repository.save(findData);
+		}
+
+		return this.insert(data);
+	}
+
+	/////////
+
+	public getRepository(): Repository<typeof Entity>{
+		return this.repository;
 	}
 }
 
--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -1,11 +1,18 @@
-import { DataSource, Entity, EntityOptions, EntityTarget, InsertResult, Repository } from "typeorm";
+import { DataSource, Entity, EntityOptions, EntityTarget, FindOneOptions, FindOptionsWhere, InsertResult, Repository } from "typeorm";
 
 interface IBaseModel {
   entity: EntityTarget<{ (options?: EntityOptions): ClassDecorator; (name?: string, options?: EntityOptions): ClassDecorator; }>;
 	dataSource: DataSource;
 	repository: Repository<typeof Entity>;
 
+	findAll: () => Promise<typeof Entity[]>;
+	findOne: (options: FindOneOptions) => Promise<typeof Entity | null>;
+	findOneBy: (where: FindOptionsWhere<typeof Entity> | FindOptionsWhere<typeof Entity>[]) => Promise<typeof Entity | null>;
   insert: (data: object) => Promise<InsertResult>;
+	insertOrUpdate: (where: FindOptionsWhere<typeof Entity> | FindOptionsWhere<typeof Entity>[], data: object) => Promise<any>; 
+	
+	getRepository: () => Repository<typeof Entity>; 
+	//update: (where: FindOptionsWhere<typeof Entity> | FindOptionsWhere<typeof Entity>[], data: object) => Promise<[]>;
 }
 
 export {
